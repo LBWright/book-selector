af30398730c09822a27122d832b4eb1761fa9399
LBWright <lwright.js@gmail.com>
2018-04-25 16:04:15 -0600

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        { title: 'Lies of Locke Lamora' },
+        { title: 'Harry Potter'},
+        { title: 'Lord of the Rings'},
+        { title: 'Name of the Wind'}
+    ]
+}
