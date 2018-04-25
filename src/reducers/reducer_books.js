847c13150ffef7dbe018a7b7201b08fa384d7a9c
LBWright <lwright.js@gmail.com>
2018-04-25 16:15:04 -0600

    added pages to book object.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function(){
     return [
-        { title: 'Lies of Locke Lamora' },
-        { title: 'Harry Potter'},
-        { title: 'Lord of the Rings'},
-        { title: 'Name of the Wind'}
+        { title: 'Lies of Locke Lamora', pages: 323},
+        { title: 'Harry Potter', pages: 986},
+        { title: 'Lord of the Rings', pages: 1152},
+        { title: 'Name of the Wind', pages: 894}
     ]
 }
